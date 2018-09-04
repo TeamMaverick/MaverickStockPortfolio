b4aa11f0627c0ca57a0da968e9e1a6f6858d870c
Alan Yang <yangalan17@gmail.com>
2018-09-04 17:12:00 -0400

    (fix) Updated package.json and added db folder

---
+++ database/index.js
@@ -0,0 +1,21 @@
+// Boilerplate code for DB
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'FILL_ME_IN',
+  database: 'test'
+});
+
+var selectAll = function(callback) {
+  connection.query('SELECT * FROM items', function(err, results, fields) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+module.exports.selectAll = selectAll;
