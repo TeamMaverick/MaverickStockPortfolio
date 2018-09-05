c5c1418d61b69af602d19411e358a3ad7bc94c25
Alan Yang <yangalan17@gmail.com>
2018-09-05 11:33:51 -0400

    Added basic stockChart component outline, and dependencies (not functional)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,39 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import AddStock from './components/AddStock.jsx'
+import AddStock from './components/AddStock.jsx';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import StockChart from './components/StockChart.jsx';
 
 class App extends React.Component {
   constructor(props) {
-  	super(props)
-  	this.state = {
-      stocks : []
+    super(props);
+    this.state = {
+      stocks: []
     };
     this.getStocks = this.getStocks.bind(this);
     this.setStocks = this.setStocks.bind(this);
   }
-  componentDidMount(){
-    this.getStocks()
+  componentDidMount() {
+    this.getStocks();
   }
-  getStocks(){
-    axios.get('stocks')
-      .then(({data}) => {
+  getStocks() {
+    axios
+      .get('stocks')
+      .then(({ data }) => {
         console.log(data);
         setStocks(data);
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
   }
-  setStocks (stocks) {
+  setStocks(stocks) {
     this.setState({
-      stocks : stocks
-    })
+      stocks: stocks
+    });
   }
-  render () {
-  	return (
+  render() {
+    return (
       <div className="app">
-        <header className="navbar"><h1>Stock Portfolio</h1></header> 
+        <header className="navbar">
+          <h1>Stock Portfolio</h1>
+        </header>
         <div className="main">
           <AddStock getStocks={this.getStocks} />
         </div>
