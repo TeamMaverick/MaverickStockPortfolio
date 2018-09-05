2f80f48b6bf6a7fc0287e80121aa6331560974bd
Laura Zhang <lauraazee@gmail.com>
2018-09-05 15:57:15 -0400

    added react components for list of stocks

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import AddStock from './components/AddStock.jsx'
+import ListOfStocks from './components/ListOfStocks.jsx'
 
 class App extends React.Component {
   constructor(props) {
   	super(props)
   	this.state = {
-      stocks : []
+      stocks : [],
+      currentStock : {},
     };
     this.getStocks = this.getStocks.bind(this);
     this.setStocks = this.setStocks.bind(this);
+    this.displayStock = this.displayStock.bind(this);
   }
   componentDidMount(){
-    this.getStocks()
+    this.getStocks();
   }
+
+  //gets all the stocks for the user stored in the database
   getStocks(){
-    axios.get('stocks')
+    axios.get('/stocks')
       .then(({data}) => {
         console.log(data);
         setStocks(data);
@@ -25,20 +30,34 @@ class App extends React.Component {
         console.log(err);
       })
   }
+  
   setStocks (stocks) {
     this.setState({
       stocks : stocks
     })
   }
-  render () {
+  
+  //called when a ticker symbol on the stocks list is clicked
+  //requests the data for that ticker symbol and deposits it in the state
+  displayStock(stock) {
+    return axios.get('/stockInfo', { params: { stock: stock}})
+    .then((data) => {
+      this.setState({currentStock : data})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+  
+  render() {
   	return (
       <div className="app">
         <header className="navbar"><h1>Stock Portfolio</h1></header> 
         <div className="main">
           <AddStock getStocks={this.getStocks} />
+          <ListOfStocks stocksArray={this.state.stocks} displayStock={this.displayStock}/>
         </div>
-      </div>
-    );
+      </div>);
   }
 }
 
