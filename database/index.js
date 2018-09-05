d484fb2552abca9cc6360bab1b8b0e39bbd3d927
Alan Yang <yangalan17@gmail.com>
2018-09-04 20:09:10 -0400

    (feat) Set up AWS RDS MySQL connection

--- database/index.js
+++ database/index.js
@@ -1,21 +1,19 @@
 // Boilerplate code for DB
 var mysql = require('mysql');
+var { dbConfig } = require('../config.js');
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'FILL_ME_IN',
-  database: 'test'
-});
+// save to config file
+var connection = mysql.createPool(dbConfig);
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
+// var selectAll = function(callback) {
+//   connection.query('SELECT * FROM items', function(err, results, fields) {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
 
-module.exports.selectAll = selectAll;
+// module.exports.selectAll = selectAll;
+module.exports.connection = connection;
