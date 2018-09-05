3d7575db3ff850c15101c150b681c09a007921f3
Alan Yang <yangalan17@gmail.com>
2018-09-04 20:31:56 -0400

    (feat) Added example config file

---
+++ config.example.js
@@ -0,0 +1,15 @@
+// follow this structure, we will share actual config file internally
+module.exports = {
+  dbConfig: {
+    host: 'example.com',
+    user: 'student',
+    password: 'student',
+    port: '3000',
+    database: 'student'
+  },
+  example_api_key: 'abcdefg',
+  example_object: {
+    prop1: 'hello',
+    prop2: 'world'
+  }
+};
