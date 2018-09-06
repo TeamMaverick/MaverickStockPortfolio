703deb3308ab903d49d815a93ca598e43483f416
miltonlopezjr <miltonathm@hotmail.com>
2018-09-06 16:00:38 -0400

    styling for the healthcheck

--- client/src/components/HealthCheckItem.jsx
+++ client/src/components/HealthCheckItem.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 
 const HealthCheckItem = (props) => {
   return (
-    <div className="container">
-      <h2>{props.stock.ticker}</h2>
-    </div>
-    
+    <nav className="level is-mobile">
+      <div className="level-item has-text-centered">
+        <div>
+          <p className="heading">Ticker</p>
+          <p className="title">{props.stock.ticker}</p>
+        </div>
+      </div>
+      <div className="level-item has-text-centered">
+        <div>
+          <p className="heading">Today's Change</p>
+          <p className="title"><i className="fas fa-arrow-down"></i> 123</p>
+        </div>
+      </div>
+      <div className="level-item has-text-centered">
+        <div>
+          <p className="heading">Year-to-Date</p>
+          <p className="title">456K</p>
+        </div>
+      </div>
+    </nav>
   )
 }
 
