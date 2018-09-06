47b58665132e957275001534f8febc9fe995fd1a
Alan Yang <yangalan17@gmail.com>
2018-09-06 11:32:10 -0400

    (update) changed stock to include quantity

--- database/stockSchema.sql
+++ database/stockSchema.sql
@@ -2,17 +2,12 @@ DROP DATABASE IF EXISTS usersStocks;
 CREATE DATABASE usersStocks;
 
 USE usersStocks;
-DROP TABLE IF EXISTS stock, user;
+DROP TABLE IF EXISTS stock;
 
 CREATE TABLE stock (
   id INT NOT NULL AUTO_INCREMENT,
   stock_ticker VARCHAR(20) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(200) NOT NULL,
-  stock_id INT NOT NULL,
+  quantity INT NOT NULL,
+  UNIQUE (stock_ticker),
   PRIMARY KEY (id)
 );
