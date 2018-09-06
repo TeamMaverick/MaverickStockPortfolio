a862196774b6449020bc8e1103e248088bdeccfe
Laura Zhang <lauraazee@gmail.com>
2018-09-06 11:26:30 -0400

    cleaned up and commented list of stocks

--- client/src/components/ListOfStocks.jsx
+++ client/src/components/ListOfStocks.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import StockListItem from './StockListItem.jsx';
 
+//List component that displays all the stocks the user has saved
 var ListOfStocks = function(props) {
   return (
     <div>
