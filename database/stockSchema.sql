d7ea9b72dfd7c6f7802024401f5dac11ed475173
Alan Yang <yangalan17@gmail.com>
2018-09-06 12:00:07 -0400

    test

--- database/stockSchema.sql
+++ database/stockSchema.sql
@@ -7,19 +7,7 @@ DROP TABLE IF EXISTS stock;
 CREATE TABLE stock (
   id INT NOT NULL AUTO_INCREMENT,
   stock_ticker VARCHAR(20) NOT NULL,
-<<<<<<< HEAD
   quantity INT NOT NULL,
   UNIQUE (stock_ticker),
   PRIMARY KEY (id)
 );
-=======
-  PRIMARY KEY (id)
-);
-
--- CREATE TABLE user (
---   id INT NOT NULL AUTO_INCREMENT,
---   username VARCHAR(200) NOT NULL,
---   stock_id INT NOT NULL,
---   PRIMARY KEY (id)
--- );
->>>>>>> dev
