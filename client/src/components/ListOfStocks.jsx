da66615368efd7e40dd8f99bad79a22ea17a8b7f
Laura Zhang <lauraazee@gmail.com>
2018-09-10 15:33:08 -0400

    finished total

--- client/src/components/ListOfStocks.jsx
+++ client/src/components/ListOfStocks.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import StockListItem from './StockListItem.jsx';
 
 //List component that displays all the stocks the user has saved
-var ListOfStocks = function(props) {
+var ListOfStocks = function({ stocksArray, removeCheckedBoxes, displayStock, calculateTotal }) {
   return (
       <div className="section">
-        {props.stocksArray.map((stock) => {
-          return <StockListItem stock={stock} key={stock.ticker} displayStock={props.displayStock} />;
+        {stocksArray.map((stock) => {
+          return <StockListItem stock={stock} key={stock.ticker} displayStock={displayStock} />;
         })}
-        <button className="button" onClick={(evt) => props.removeCheckedBoxes(evt)}>Remove Checked Stocks</button>
+        <div className="total price">Total : {calculateTotal(stocksArray)}</div>
+        <button className="button" onClick={(evt) => removeCheckedBoxes(evt)}>Remove Checked Stocks</button>
       </div>
   );
 };
