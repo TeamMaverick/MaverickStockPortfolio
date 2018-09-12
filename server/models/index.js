bd1af3cdc88e2e1e823b253fe873a66aaa7f175a
Laura Zhang <lauraazee@gmail.com>
2018-09-12 11:24:36 -0400

    postTickersAndNames function added back

--- server/models/index.js
+++ server/models/index.js
@@ -90,4 +90,17 @@ module.exports = {
       }
     });
   },
+
+  //inserts tickers and their company names into the tickersAndNames table in the database
+  postTickersAndNames: function(stockArray) {
+    stockArray.forEach((stock) => {
+      const params = [stock.symbol, stock.name];
+      const insertQuery = `INSERT INTO tickersAndNames (stock_ticker, company_name) VALUES (?, ?)`;
+      db.query(insertQuery, params, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    })
+  }
 };
