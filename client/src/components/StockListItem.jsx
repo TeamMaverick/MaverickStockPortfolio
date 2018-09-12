d66972e3046d4f5acd9879ff21ae2d06880e1cae
Alan Yang <yangalan17@gmail.com>
2018-09-12 12:16:24 -0400

    (feat/piechart) completed basic version

--- client/src/components/StockListItem.jsx
+++ client/src/components/StockListItem.jsx
@@ -4,15 +4,13 @@ import React from 'react';
 var StockListItem = function({ stock, displayStock }) {
   //displayStock here is the clickhandler function defined in the app that makes a get request
   //to get the data for that particular stock and deposits it in the app's state
-  console.log(stock);
   return (
     <div className="StockListItem">
       <div onClick={() => displayStock(stock.ticker)}>
-      <input className="checkedStock checkbox" value={stock.ticker} type="checkbox" />
+        <input className="checkedStock checkbox" value={stock.ticker} type="checkbox" />
         {`${stock.ticker} : ${stock.companyName} : ${stock.quantity} : 
         ${stock.price} : ${stock.price * stock.quantity}`}
       </div>
-      
     </div>
   );
 };
