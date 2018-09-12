d462e8c6aabd5978fea1e3cc815f207f4dcffe05
miltonlopezjr <miltonathm@hotmail.com>
2018-09-12 16:37:11 -0400

    inital pass at styling updates

--- client/src/components/ListOfStocks.jsx
+++ client/src/components/ListOfStocks.jsx
@@ -25,7 +25,7 @@ var ListOfStocks = function({ stocksArray, removeCheckedBoxes, displayStock, por
         {stocksArray.map((stock) => {
           return <StockListItem stock={stock} key={stock.ticker} displayStock={displayStock} getStocks={getStocks} />;
         })}
-        <div className="total price">Portfolio Total : {portfolioTotal}</div>
+        <div className="totalprice">Portfolio Total : {portfolioTotal}</div>
         <button className="button" onClick={(evt) => removeCheckedBoxes(evt)}>Remove Checked Stocks</button>
       </div>
   );
