302de57c697488444fc4a07a71502ea47175adfa
miltonlopezjr <miltonathm@hotmail.com>
2018-09-13 22:51:20 -0400

    finished polishing

--- client/src/components/HealthCheck.jsx
+++ client/src/components/HealthCheck.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import Axios from 'axios';
 import HealthCheckItem from './HealthCheckItem.jsx';
+import StockChart from './StockChart.jsx';
+
 
 class HealthCheck extends React.Component {
   constructor(props) {
     super(props);
-    this.getStockInfo = this.getStockInfo.bind(this);
     this.state = {
-      stocksData: ''
+      stocksData: '',
+      currentStock: {},
+      apiWait : false
     };
+    this.getStockInfo = this.getStockInfo.bind(this);
+    this.displayStock = this.displayStock.bind(this);
+
   }
 
   // call getstockinfo for all stocks passed down in props
   componentDidMount() {
     this.getStockInfo();
+
+    if(this.props.stocks.length > 0){
+      this.displayStock(this.props.stocks[0].stock_ticker);
+    } else {
+      this.displayStock('MSFT');
+    }
+  }
+
+ //called when a ticker symbol on the stocks list is clicked
+  //requests the data for that ticker symbol and deposits it in the state
+  displayStock(stock) {
+    return Axios
+      .get('/api/stockInfo', { params: { STOCK: stock } })
+      .then(({ data }) => {
+        if(data.Information){
+          this.setState({ apiWait : true});
+        } else {
+          this.setState({ currentStock: data, apiWait : false });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   // call stock API to get realtime info for stocks
@@ -38,21 +67,31 @@ class HealthCheck extends React.Component {
 
   render() {
     return (
-      <div className="HealthCheck">
-        <h1 className="healthCheckHeader">Health Check</h1>
-        <div>
-          {/* <p>Let's do a quick health check on your {this.props.stocks.length} Stocks</p> */}
-          {this.state.stocksData
-            ? this.props.stocks.map((stock, i) => {
-                return (
-                  <HealthCheckItem
-                    key={i}
-                    stockInfo={this.state.stocksData[stock.stock_ticker]}
-                    displayStock={this.props.displayStock}
-                  />
-                );
-              })
-            : ''}
+      <div className="columns">
+        <div className="column check">
+          <div className="HealthCheck">
+            <h1 className="healthCheckHeader">Health Check</h1>
+            <div>
+              {/* <p>Let's do a quick health check on your {this.props.stocks.length} Stocks</p> */}
+              {this.state.stocksData
+                ? this.props.stocks.map((stock, i) => {
+                    return (
+                      <HealthCheckItem
+                        key={i}
+                        stockInfo={this.state.stocksData[stock.stock_ticker]}
+                        displayStock={this.displayStock}
+                      />
+                    );
+                  })
+                : ''}
+            </div>
+          </div>
+        </div> 
+        <div className="column is-two-thirds is-centered">
+          {this.state.apiWait ? (<div className="apiWait"><i className="fas fa-pause"></i> Please wait.. API only can we called few times..</div>) : ''}
+          {this.state.currentStock.metaData === undefined ? null : (
+            <StockChart currentStock={this.state.currentStock} />
+          )}
         </div>
       </div>
     );
