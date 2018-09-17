698495d603e273bc0fef7a56395d225a39a9457f
miltonlopezjr <miltonathm@hotmail.com>
2018-09-16 21:46:51 -0400

    adding unit test for stock creation

--- server/index.js
+++ server/index.js
@@ -1,21 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const path = require('path');
-// sets process.env variables
-const config = require('dotenv').config();
-const stockRoutes = require('./routes/index.js');
-//Helpers
-// const apiHelpers = require('./helpers/apiHelpers.js');
-
-//Middleware
-app.use(bodyParser.json());
-
-// server client side
-app.use(express.static(path.join(__dirname, '/../client/dist')));
-
-// Set stock api prefix
-app.use('/api', stockRoutes);
+const app = require('./maverick.js');
 
 app.listen(process.env.PORT, function() {
   console.log(`listening on port ${process.env.PORT}!`);
