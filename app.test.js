f9b001708e8821aeff139816e364c46ad10b80fb
Jay <hyunjae9034@gmail.com>
2018-09-22 11:27:42 -0400

    created two testing for sockets

--- app.test.js
+++ app.test.js
@@ -3,6 +3,9 @@ import renderer from 'react-test-renderer'
 import React from 'react'
 import Search from './client/src/components/Search'
 import Adapter from 'enzyme-adapter-react-16';
+import Chat from './server/index.js';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:3000')
 
 configure({ adapter: new Adapter() });
 
@@ -11,7 +14,6 @@ test('testing snapshot', () => {
   const component = renderer.create(<Search />)
   let comp = component.toJSON();
   expect(comp).toMatchSnapshot();
-
 })
 
 test('testing matcher', () => {
@@ -21,4 +23,46 @@ test('testing matcher', () => {
 test('testing state', () => {
   const wrapper = shallow(<Search />);
   expect(wrapper.state('checked')).toEqual(0)
+})
+
+test('testing if the message was sent by the correct username', (done) => {
+  let obj = {
+    username: 'Jay',
+    message: 'Hello World'
+  }
+  
+  socket.emit('chat', obj)
+  let fetchData = (cb) => {
+    socket.on('chat', (data) => {
+      cb(data);
+    })
+  }
+
+  function callback(data) {
+    expect(obj.username).toBe(data.username);
+    done();
+  }
+
+  fetchData(callback);
+})
+
+test('testing if the message was sent correctly', (done) => {
+  let obj = {
+    username: 'Jay',
+    message: 'Hello World'
+  }
+  
+  socket.emit('chat', obj)
+  let fetchData = (cb) => {
+    socket.on('chat', (data) => {
+      cb(data);
+    })
+  }
+
+  function callback(data) {
+    expect(obj.message).toBe(data.message);
+    done();
+  }
+
+  fetchData(callback);
 })
